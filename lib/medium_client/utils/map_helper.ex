2861cf2ef1434e13afe317a6b1a6506ded5e9d1e
Roberto Dip <dip.jesusr@gmail.com>
2016-11-22 14:45:24 -0300

    Adds the Utils module and helper functions

---
+++ lib/medium_client/utils/map_helper.ex
@@ -0,0 +1,31 @@
+defmodule MediumClient.Utils.MapHelper do
+  @moduledoc """
+    Utility functions to deal with Maps
+  """
+
+  @doc """
+  Given a Map with keys of type String, returns a Map with
+  keys of type Atom.
+
+  This is specially useful to parse JSON responses.
+
+  ## Examples
+
+    string_to_atom_keys(%{"one" => 1}) //=> %{one: 1}
+  """
+
+  @spec string_to_atom_keys(Map.t) :: Map
+  def string_to_atom_keys(map) when is_map(map) do
+    for {key, val} <- map,
+      into: %{},
+      do: {String.to_atom(key), string_to_atom_keys(val)}
+  end
+
+  @spec string_to_atom_keys(List.t) :: List
+  def string_to_atom_keys(list) when is_list(list) do
+    list
+    |> Enum.map(&string_to_atom_keys/1)
+  end
+
+  def string_to_atom_keys(val), do: val
+end
---
+++ test/utils/map_helper.ex
@@ -0,0 +1,31 @@
+defmodule MediumClient.Utils.MapHelperTest do
+  use ExUnit.Case, async: true
+
+  alias MediumClient.Utils.MapHelper
+
+  @moduletag utils: :map_helper
+
+  test "#string_to_atom_keys converts all string keys to atoms" do
+    string_keys = %{"one" => 1, "two" => 2}
+    atom_keys = MapHelpers.string_to_atom_keys string_keys
+
+    assert atom_keys.one == 1
+    assert atom_keys.two == 2
+  end
+
+  test "#string_to_atom_keys works with nested Maps" do
+    string_keys = %{"one" => 1, "two" => %{"three" => 3}}
+    atom_keys = MapHelpers.string_to_atom_keys string_keys
+
+    assert atom_keys.one == 1
+    assert atom_keys.two.three == 3
+  end
+
+  test "#string_to_atom_keys works with nested Lists" do
+    string_keys = %{"one" => 1, "two" => [%{"three" => 3}]}
+    atom_keys = MapHelpers.string_to_atom_keys string_keys
+
+    assert atom_keys.one == 1
+    assert atom_keys.two == [%{three: 3}]
+  end
+end
