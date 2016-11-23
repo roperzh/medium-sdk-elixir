36ab5fef9ac7643e713191d4f8368bb3393d19f2
Roberto Dip <dip.jesusr@gmail.com>
2016-11-23 19:46:31 -0300

    Fix the tests of the map helper module

--- test/utils/map_helper_test.exs
+++ test/utils/map_helper_test.exs
@@ -7,7 +7,7 @@ defmodule Medium.Utils.MapHelperTest do
 
   test "#string_to_atom_keys converts all string keys to atoms" do
     string_keys = %{"one" => 1, "two" => 2}
-    atom_keys = MapHelpers.string_to_atom_keys string_keys
+    atom_keys = MapHelper.string_to_atom_keys string_keys
 
     assert atom_keys.one == 1
     assert atom_keys.two == 2
@@ -15,7 +15,7 @@ defmodule Medium.Utils.MapHelperTest do
 
   test "#string_to_atom_keys works with nested Maps" do
     string_keys = %{"one" => 1, "two" => %{"three" => 3}}
-    atom_keys = MapHelpers.string_to_atom_keys string_keys
+    atom_keys = MapHelper.string_to_atom_keys string_keys
 
     assert atom_keys.one == 1
     assert atom_keys.two.three == 3
@@ -23,7 +23,7 @@ defmodule Medium.Utils.MapHelperTest do
 
   test "#string_to_atom_keys works with nested Lists" do
     string_keys = %{"one" => 1, "two" => [%{"three" => 3}]}
-    atom_keys = MapHelpers.string_to_atom_keys string_keys
+    atom_keys = MapHelper.string_to_atom_keys string_keys
 
     assert atom_keys.one == 1
     assert atom_keys.two == [%{three: 3}]
