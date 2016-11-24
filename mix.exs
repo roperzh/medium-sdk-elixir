b73a2c91c6f1c39baab3620f76a81c57b750f3ea
Roberto Dip <dip.jesusr@gmail.com>
2016-11-24 12:54:09 -0300

    Configures mix to load cases while testing :dancers:

--- mix.exs
+++ mix.exs
@@ -6,6 +6,7 @@ defmodule Medium.Mixfile do
       app: :medium,
       version: "0.1.0",
       elixir: "~> 1.3",
+      elixirc_paths: elixirc_paths(Mix.env),
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps: deps()
@@ -17,6 +18,10 @@ defmodule Medium.Mixfile do
       applications: [:logger, :hackney]
     ]
   end
+
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/cases"]
+  defp elixirc_paths(_),     do: ["lib"]
 
   defp deps do
     [
